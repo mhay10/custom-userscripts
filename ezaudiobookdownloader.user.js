258f73e7244132b638223f103eb75cb4aa3f243b
mhay10 <maxwellhay1@gmail.com>
2026-01-02 18:41:53 -0700

    Enhance script description and logging; fix track element retrieval

--- ezaudiobookdownloader.user.js
+++ ezaudiobookdownloader.user.js
@@ -1,52 +1,69 @@
 // ==UserScript==
 // @name         Download EzAudiobooksForSoul Audiobooks
 // @namespace    https://github.com/mhay10/custom-userscripts
-// @updateURL    https://github.com/mhay10/custom-userscripts/raw/main/ezaudiobookdownloader.user.js
 // @version      1.0
-// @description  Download audiobooks from EzAudiobooksForSoul
+// @description  Download audiobooks from EzAudiobooksForSoul and similar sites
 // @author       mhay10
 // @match        https://ezaudiobookforsoul.com/audiobook/*
+// @match        https://audiobooks4soul.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=ezaudiobookforsoul.com
 // @grant        none
+// @updateURL    https://github.com/mhay10/custom-userscripts/raw/main/ezaudiobookdownloader.user.js
+// @downloadURL  https://github.com/mhay10/custom-userscripts/raw/main/ezaudiobookdownloader.user.js
 // ==/UserScript==
 
 (async function () {
     "use strict";
 
     // Wait for loading to finish
+    console.log("Waiting for loading to finish...");
     await waitForLoadFinish();
+    console.log("Loading finished. Starting...");
 
+    // Get track elemnts from playlist
     const player = document.querySelector("#audio_content");
-    console.log(player);
+    const playlist = player.querySelector(".simp-playlist");
+    const trackElements = playlist.querySelectorAll(".simp-source");
+
+    // Iterate through tracks and wait for audio to load
+    const trackUrls = [];
+    for (const track of trackElements) {
+        // Load track url by simulating click
+        track.click();
+        await waitForAudioLoad(track);
+
+        // Get track url
+        const trackUrl = track.getAttribute("data-src");
+        trackUrls.push(trackUrl);
+    }
+    console.log("All track URLs:", trackUrls);
 })();
 
-async function decrypt(encrypted) {
-    const baseUrl =
-        "https://ezaudiobookforsoul.com/wp-content/plugins/custom-story-audio/inc/security/decrypt.php";
-    const response = await fetch(
-        `${baseUrl}?encrypted=${encodeURI(encrypted)}`,
-        {
-            credentials: "include",
-            headers: {
-                "User-Agent":
-                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:146.0) Gecko/20100101 Firefox/146.0",
-                Accept: "*/*",
-                "Accept-Language": "en-US,en;q=0.5",
-                "Alt-Used": "ezaudiobookforsoul.com",
-                "Sec-Fetch-Dest": "empty",
-                "Sec-Fetch-Mode": "cors",
-                "Sec-Fetch-Site": "same-origin",
-                "Sec-GPC": "1",
-                Priority: "u=0",
-            },
-            referrer:
-                "https://ezaudiobookforsoul.com/audiobook/blood-of-tyrants-audiobook/",
-            method: "GET",
-            mode: "cors",
-        }
-    );
+async function waitForAudioLoad(track) {
+    return new Promise(function (resolve) {
+        // Create mutation observer for audio element
+        const observer = new MutationObserver(function (mutations) {
+            for (const mutation of mutations) {
+                // Check audio element src change
+                if (
+                    mutation.type == "attributes" &&
+                    mutation.attributeName == "data-src"
+                ) {
+                    // Resolve promise once src is updated
+                    const trackUrl = track.getAttribute("data-src");
+                    console.log("Audio loaded:", trackUrl);
+                    observer.disconnect();
+                    resolve(trackUrl);
+                }
+            }
+        });
 
-    return response;
+        // Start observing audio element for changes
+        observer.observe(track, {
+            attributes: true,
+            attributeOldValue: true,
+        });
+    });
 }
 
 async function waitForLoadFinish() {
@@ -63,8 +80,7 @@ async function waitForLoadFinish() {
                     // Make sure loading message is hidden
                     const newStyle = mutation.target.getAttribute("style");
                     if (newStyle.includes("display: none")) {
-                        // Loading finished
-                        console.log("Loading complete, executing script...");
+                        // Resolve promise and stop observing once loaded
                         observer.disconnect();
                         resolve();
                     }
