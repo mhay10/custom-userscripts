814363fcf6af615c89b2c5f3178831228bf786c1
mhay10 <maxwellhay1@gmail.com>
2026-09-08 00:46:54 -0600

    Finish repo list lookups. Fix a bunch of logic bugs

--- better-github-dashboard/better-github-dashboard.user.js
+++ better-github-dashboard/better-github-dashboard.user.js
@@ -12,51 +12,19 @@
 // @run-at       document-end
 // ==/UserScript==
 
-(function () {
+(async function () {
     "use strict";
-
-    // ==========================================
-    //                 UTILITIES
-    // ==========================================
-
-    const logger = {
-        info: (msg) =>
-            console.log(`%c[Better GH Dashboard]%c ${msg}`, "color: #00a2ff;"),
-        error: (msg) =>
-            console.log(`%c[Better GH Dashboard]%c ${msg}`, "color: #ff2c3a;"),
-    };
-
-    function debounce(fn, ms) {
-        let timer;
-        return function (...args) {
-            clearTimeout(timer);
-            timer = setTimeout(() => fn.apply(this, args), ms);
-        };
-    }
-
-    function getCspNonce() {
-        // Try to find it in a meta tag
-        const metaNonce = document
-            .querySelector('meta[name="csp-nonce"]')
-            ?.getAttribute("content");
-        if (metaNonce) return metaNonce;
-
-        // Try to find it from a script tag
-        const scriptNonce = document.querySelector("script[nonce]")?.nonce;
-        if (scriptNonce) return scriptNonce;
-
-        return null;
-    }
 
     // ==========================================
     //           CONFIGURATION & STATE
     // ==========================================
     const config = {
         selectors: {
-            dashboard: "#dashboard",
-            repoList: ".js-dashboard-repos-list",
-            copilot: ".copilotPreview__container",
-            paginationForm: "",
+            repoList: "ul.js-dashboard-repos-list",
+            repoPaginationForm:
+                "form.js-ajax-pagination, form.js-more-repos-form",
+            copilotElems:
+                '.copilotPreview__container, react-partial[partial-name*="copilot"]',
         },
         delays: {
             mutObserverDebounce: 150,
@@ -77,6 +45,62 @@
     };
 
     // ==========================================
+    //                 UTILITIES
+    // ==========================================
+
+    const logger = {
+        info: (msg) =>
+            console.log(`%c[Better GH Dashboard]%c ${msg}`, "color: #74E4EE;"),
+        error: (msg) =>
+            console.log(`%c[Better GH Dashboard]%c ${msg}`, "color: #EE3B60;"),
+    };
+
+    function delay(ms) {
+        return new Promise((resolve) => setTimeout(resolve, ms));
+    }
+
+    function debounce(fn, ms) {
+        let timer;
+        return function (...args) {
+            clearTimeout(timer);
+            timer = setTimeout(() => fn.apply(this, args), ms);
+        };
+    }
+
+    function getCspNonce() {
+        // Try to get nonce from a meta tag
+        const metaNonce = document
+            .querySelector('meta[name="csp-nonce"]')
+            ?.getAttribute("content");
+        if (metaNonce) return metaNonce;
+
+        // Try to get nonce from a script tag
+        const scriptNonce = document.querySelector("script[nonce]")?.nonce;
+        if (scriptNonce) return scriptNonce;
+
+        return null;
+    }
+
+    function setCurrentUser() {
+        // Don't re-set if current user has already been set
+        if (state.currentUser) return;
+
+        // Try to find current user from a meta tag
+        const currentUser = document
+            .querySelector('meta[name="user-login"]')
+            ?.getAttribute("content")
+            ?.toLowerCase();
+
+        // Set state's current user if found
+        if (currentUser) {
+            state.currentUser = currentUser;
+            return true;
+        }
+
+        return false;
+    }
+
+    // ==========================================
     //              COPILOT REMOVAL
     // ==========================================
 
@@ -91,7 +115,7 @@
 
         // Set style contents to hide the copilot widget
         style.textContent = `
-            ${config.selectors.copilot}, ${config.selectors.copilot} * {
+            ${config.selectors.copilotElems}, ${config.selectors.copilotElems} * {
                 display: none !important;
                 visibility: hidden !important;
                 height: 0 !important;
@@ -107,16 +131,99 @@
     //             REPOSITORY SIDEBAR
     // ==========================================
 
-    function parseRepoNode() {
-        /* ... */
+    function parseRepoNode(node) {
+        // Get link conatiner
+        const linkElem = node.querySelector(
+            'a[data-hovercard-type="repository"]',
+        );
+        if (!linkElem) return null;
+
+        // Cleanup URL to not be absolute or have a leading slash
+        const rawHref = linkElem.getAttribute("href") || "";
+        const cleanPath = rawHref
+            .replace(/^https?:\/\/[^\/]+/, "") // Remove https:// url portion
+            .replace(/^\//, ""); // Remove starting slashes
+
+        // Split URL into parts and make sure that enough exist
+        const parts = cleanPath.split("/");
+        if (parts.length < 2) return null;
+        const repoOwner = parts[0];
+        const repoName = parts[1];
+
+        // Get avatar image src
+        const imgElem = node.querySelector("img.avatar, img.avatar-small");
+        const avatarSrc = imgElem ? imgElem.getAttribute("src") : "";
+
+        // Check if repo is a peronal repo
+        const isPersonal =
+            state.currentUser &&
+            state.currentUser.toLowerCase() == repoOwner.toLowerCase();
+
+        // Return parsed object
+        return {
+            repoName: repoName,
+            repoOwner: repoOwner,
+            repo: `${repoOwner}/${repoName}`.toLowerCase(), // Force lowercase for deduplication
+            category: isPersonal ? "Personal" : repoOwner,
+            avatarSrc: avatarSrc,
+        };
     }
 
-    function renderDropdowns() {
+    function renderRepoDropdowns() {
         /* ... */
     }
 
     async function fetchRemainingPages() {
-        /* ... */
+        // Don't request more repos if already being requested
+        if (state.isFetching || state.paginationComplete) return;
+
+        // Start fetch rest of the pages
+        state.isFetching = true;
+
+        // Loop while the form cursor exists
+        let cursor = document
+            .querySelector('input[name="repos_cursor"]')
+            ?.getAttribute("value");
+        for (let pageCount = 1; pageCount < 50 && cursor; pageCount++) {
+            // Wait to avoid rate limiting
+            await delay(config.delays.paginationFetch);
+
+            // Send request to get next page of repos
+            const url = "/dashboard/ajax_my_repositories";
+            const params = new URLSearchParams({
+                location: "left",
+                repos_cursor: cursor,
+            });
+            const response = await fetch(`${url}?${params}`, {
+                headers: { "X-Requested-With": "XMLHttpRequest" },
+            });
+
+            // Stop fetching if the response returns an error
+            if (!response.ok) break;
+
+            // Parse HTML from response
+            const html = new DOMParser().parseFromString(
+                await response.text(),
+                "text/html",
+            );
+
+            // Get all repo elements in HTML and parse them
+            html.querySelectorAll("li").forEach((li) => {
+                const data = parseRepoNode(li);
+                if (data) state.repos.set(data.repo, data);
+            });
+
+            // Check if another form cursor exists and update the UI
+            cursor = html
+                .querySelector('input[name="repos_cursor"]')
+                ?.getAttribute("value");
+            renderRepoDropdowns();
+        }
+
+        // Update fetch state flags and do one final render call
+        state.isFetching = false;
+        state.paginationComplete = true;
+        renderRepoDropdowns();
     }
 
     // ==========================================
@@ -135,10 +242,26 @@
     //         INITIALIZATION & OBSERVER
     // ==========================================
 
-    logger.info("Starting to modify dashboard");
+    await delay(1000);
+
+    if (!setCurrentUser()) {
+        logger.error("Current is not logged in. Stopping");
+        return;
+    }
+
+    logger.info(`Current User: ${state.currentUser}`);
+    logger.info("Starting to modify dashboard...");
+
+    logger.info("Setting current user...");
 
     // TODO: Insert Main logic here
+    logger.info("Hiding Copilot widget...");
     hideCopilot();
+
+    logger.info("Fetching all repositories...");
+    await fetchRemainingPages();
+
+    console.log(state.repos);
 
     logger.info("Dashboard modifications complete");
 })();
